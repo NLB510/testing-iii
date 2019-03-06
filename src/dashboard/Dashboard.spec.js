f91cad73ca4e8f5679e429147219ea509d911836
Nate Boyette <nateboyette@icloud.com>
2019-03-06 14:44:47 -0800

    Add tests if dashboard renders without errors, and if gate defaults to Unlocked and Open

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
 // Test away
+import React from 'react';
+import  {render, queryByText}from 'react-testing-library';
+import renderer from 'react-test-renderer'
+import 'react-testing-library/cleanup-after-each';
+
+import Dashboard from './Dashboard'
+
+
+describe('<Dashboard />', () => {
+
+  it('dashboard should render', () => {
+    const dashboard = renderer.create(<Dashboard />)
+    expect(dashboard.toJSON()).toMatchSnapshot();
+  })
+
+
+
+
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,36 @@
-// Test away!
+
+import React from 'react';
+import  {render, queryByText}from 'react-testing-library';
+import renderer from 'react-test-renderer'
+import 'react-testing-library/cleanup-after-each';
+
+import Display from './Display'
+
+
+describe('<Display />', () => {
+
+  
+
+
+  it('should default to unlocked', () => {
+    const {getByText} = render(<Display />);
+
+    const isLocked = getByText(/Unlocked/i).textContent
+    expect(isLocked).toEqual('Unlocked');
+    
+
+  })
+
+  it('should default to closed', () => {
+    const { getByText } = render(<Display />);
+
+    // const isLocked = getByText(/locked/i).textContent
+    const isOpen = getByText(/Open/i).textContent;
+    expect(isOpen).toEqual('Open');
+
+
+  })
+
+
+
+})
