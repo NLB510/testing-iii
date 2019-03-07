c5fd784a0c7c1900163d8f1b26db18a893d0b05b
Nate Boyette <nateboyette@icloud.com>
2019-03-07 00:20:16 -0800

    Add tests for button controls and button text based on the different states of the gate

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -13,9 +13,9 @@ describe('<Controls />', () => {
   it('should fire lock gate button if gate is closed', () => {
     const {getByText} = render(<Controls closed={true} locked={false} toggleLocked={mockFunction}/>)
 
-    const lockButton = getByText(/Lock Gate/i)
+    const lockBtn = getByText(/Lock Gate/i)
 
-    fireEvent.click(lockButton);
+    fireEvent.click(lockBtn);
 
     expect(mockFunction).toHaveBeenCalled();
 
@@ -33,6 +33,49 @@ describe('<Controls />', () => {
 
   })
 
+
+  it('button should display "Lock Gate" if gate is closed and unlocked', () => {
+    const { getByText } = render(<Controls closed={true} locked={false} />)
+
+    expect(getByText('Lock Gate')).toBeInTheDocument();
+
+
+  })
+
+  it('button should display "Unlock Gate" if gate is closed and locked', () => {
+    const {getByText} = render(<Controls closed={true} locked={true} />)
+
+    expect(getByText('Unlock Gate')).toBeInTheDocument();
+
+
+  })
+
+  it('should default to a disabled lock button display "Close Gate" on gate button', () => {
+
+    const { getByText } = render(<Controls closed={false} locked={false} />)
+
+    const lockBtn = getByText(/Lock Gate/i);
+    
+
+    expect(lockBtn.disabled).toBeTruthy();
+    expect(getByText(/Close Gate/i)).toBeInTheDocument();
+
+  })
+  
+  it('should disable gate button and display "Unlock Gate" on lock button if the gate is locked and closed', () => {
+
+    const {getByText} = render(<Controls closed={true} locked={true} />)
+  
+    
+
+    const gateBtn = getByText(/Open Gate/i);
+
+    expect(getByText('Unlock Gate')).toBeInTheDocument();
+    expect(gateBtn.disabled).toBeTruthy();
+
+  })
+
+  
 
 
 
